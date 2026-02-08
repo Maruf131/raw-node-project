b1612739039101c60c6c47259bd39327a8291ff9
Maruf131 <mdsheikhmaruf131@gmail.com>
2026-02-08 13:00:12 +0600

    added node environment feature

--- helpers/handleReqRes.js
+++ helpers/handleReqRes.js
@@ -1,54 +1,58 @@
 // dependencies
-const url = require('url');
-const {StringDecoder} = require('string_decoder');
-const routes = require('../routes');
-const {notFoundHandler} = require('../handlers/routeHandlers/notFoundHandler');
+const url = require("url");
+const { StringDecoder } = require("string_decoder");
+const routes = require("../routes");
+const {
+  notFoundHandler,
+} = require("../handlers/routeHandlers/notFoundHandler");
 
 // app-object module scaffolding
 const handler = {};
 
-handler.handleReqRes = (req, res) =>{
-    // request handle
-    // get the url and purse it 
-    const parsedUrl = url.parse(req.url, true);
-    const path = parsedUrl.pathname;
-    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-    const method = req.method.toLowerCase();
-    const queryStringObject = parsedUrl.query;
-    const headersObject = req.headers;
-    const requestProperties = {
-        parsedUrl,
-        path,
-        trimmedPath,
-        method,
-        queryStringObject,
-        headersObject,
-    }
+handler.handleReqRes = (req, res) => {
+  // request handle
+  // get the url and purse it
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+  const method = req.method.toLowerCase();
+  const queryStringObject = parsedUrl.query;
+  const headersObject = req.headers;
+  const requestProperties = {
+    parsedUrl,
+    path,
+    trimmedPath,
+    method,
+    queryStringObject,
+    headersObject,
+  };
+
+  const decoder = new StringDecoder("utf-8");
+  let realData = "";
+
+  const chosenHandler = routes[trimmedPath]
+    ? routes[trimmedPath]
+    : notFoundHandler;
+
+  req.on("data", (buffer) => {
+    realData += decoder.write(buffer);
+  });
+
+  req.on("end", () => {
+    realData += decoder.end();
     
-    const decoder = new StringDecoder('utf-8');
-    let realData = '';
+    chosenHandler(requestProperties, (statusCode, payload) => {
+      statusCode = typeof statusCode === "number" ? statusCode : 500;
+      payload = typeof payload === "object" ? payload : {};
 
-    const chosenHandler = routes[trimmedPath] ? routes[trimmedPath] : notFoundHandler;
-    chosenHandler(requestProperties, (statusCode, payload)=>{
-        statusCode = typeof(statusCode) === 'number' ? statusCode : 500;
-        payload = typeof(payload) === 'object' ? payload : {};
-
-        const payloadString = JSON.stringify(payload);
-        //return the final response 
-        res.writeHead(statusCode);
-        res.end(payloadString)
-    })
-
-    req.on('data', (buffer) =>{
-        realData += decoder.write(buffer);
-    })
-
-    req.on('end', ()=>{
-        realData += decoder.end();
-        console.log(realData);
-        // response handle 
-        res.end("hello programmer")
-    })
-}
+      const payloadString = JSON.stringify(payload);
+      //return the final response
+      res.writeHead(statusCode);
+      res.end(payloadString);
+    });
+    // response handle
+    res.end("hello programmer");
+  });
+};
 
 module.exports = handler;
--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 
 // dependencies
 const http = require('http');
-const {handleReqRes} = require('./helpers/handleReqRes')
+const {handleReqRes} = require('./helpers/handleReqRes');
+const environment = require('./helpers/environments');
 
 
 // app-object module scaffolding
 const app = {};
 
-// configuration
-app.config = {
-    port: 3000
-};
 
 // create server
 app.createServer = () =>{
     const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, ()=>{
-        console.log(`listening to port ${app.config.port} is running `);
+    server.listen(environment.port, ()=>{        
+        console.log(`listening to port ${environment.port} is running `);
         
     });
 }
